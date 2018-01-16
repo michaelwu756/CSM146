e5ba684795a83d5453c061f45999b91db252de38
Michael Wu <cheeserules43@gmail.com>
2018-01-15 19:48:24 -0800

    added all graphs for Problem10

--- hw0/Problem10.py
+++ hw0/Problem10.py
@@ -3,11 +3,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 mean = [0, 0]
 cov = [[1, 0], [0, 1]]
-result = np.random.multivariate_normal(mean, cov, 10)
+result = np.random.multivariate_normal(mean, cov, 1000)
 x, y=result.T
-print(result)
-print(x)
-print(y)
-plt.plot(x, y, 'x')
+plt.plot(x, y)
 plt.axis('equal')
-plt.show()
+plt.savefig("Problem10-a.png")
+plt.clf
+
+mean = [1,1]
+result = np.random.multivariate_normal(mean, cov, 1000)
+x, y=result.T
+plt.plot(x, y)
+plt.axis('equal')
+plt.savefig("Problem10-b.png")
+plt.clf
+
+mean = [0,0]
+
+result = np.random.multivariate_normal(mean, cov, 1000)
+x, y=result.T
+plt.plot(x, y)
+plt.axis('equal')
+plt.savefig("Problem10-b.png")
+
+mean = [1,1]
+cov = [[2, 0], [0, 2]]
+result = np.random.multivariate_normal(mean, cov, 1000)
+x, y=result.T
+plt.plot(x, y)
+plt.axis('equal')
+plt.savefig("Problem10-c.png")
+plt.clf
+
+cov = [[1, 0.5], [0.5, 1]]
+result = np.random.multivariate_normal(mean, cov, 1000)
+x, y=result.T
+plt.plot(x, y)
+plt.axis('equal')
+plt.savefig("Problem10-d.png")
+plt.clf
+
+cov = [[1, -0.5], [-0.5, 1]]
+result = np.random.multivariate_normal(mean, cov, 1000)
+x, y=result.T
+plt.plot(x, y)
+plt.axis('equal')
+plt.savefig("Problem10-d.png")
+plt.clf
