8c6ef29fb20a57bc1155adc05bc27bd5accab469
Michael Wu <cheeserules43@gmail.com>
2018-01-15 20:11:18 -0800

    added missing x's to scatterplots c d and e

--- hw0/Problem10.py
+++ hw0/Problem10.py
@@ -22,7 +22,7 @@ mean = [0,0]
 cov = [[2, 0], [0, 2]]
 result = np.random.multivariate_normal(mean, cov, 1000)
 x, y=result.T
-plt.plot(x, y)
+plt.plot(x, y, 'x')
 plt.axis('equal')
 plt.savefig("Problem10-c.pdf")
 plt.clf()
@@ -30,7 +30,7 @@ plt.clf()
 cov = [[1, 0.5], [0.5, 1]]
 result = np.random.multivariate_normal(mean, cov, 1000)
 x, y=result.T
-plt.plot(x, y)
+plt.plot(x, y, 'x')
 plt.axis('equal')
 plt.savefig("Problem10-d.pdf")
 plt.clf()
@@ -38,7 +38,7 @@ plt.clf()
 cov = [[1, -0.5], [-0.5, 1]]
 result = np.random.multivariate_normal(mean, cov, 1000)
 x, y=result.T
-plt.plot(x, y)
+plt.plot(x, y, 'x')
 plt.axis('equal')
 plt.savefig("Problem10-e.pdf")
 plt.clf()
