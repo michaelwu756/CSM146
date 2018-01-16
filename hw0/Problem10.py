180167bfc2512170c4ee1e07b2c24a0957ecdb89
Michael Wu <cheeserules43@gmail.com>
2018-01-15 19:36:20 -0800

    updated Problem10.py

--- hw0/Problem10.py
+++ hw0/Problem10.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python
 import matplotlib.pyplot as plt
 import numpy as np
 mean = [0, 0]
 cov = [[1, 0], [0, 1]]
-x, y = np.random.multivariate_normal(mean, cov, 1000)
+result = np.random.multivariate_normal(mean, cov, 10)
+x, y=result.T
+print(result)
+print(x)
+print(y)
 plt.plot(x, y, 'x')
 plt.axis('equal')
 plt.show()
