0c83a8bb722abea8816d200d15b3718d206185df
Michael Wu <cheeserules43@gmail.com>
2018-03-16 03:22:50 -0700

    made duplicate during init random, made kaverages to implement kmedoids

--- hw5/src/faces.py
+++ hw5/src/faces.py
@@ -25,23 +25,23 @@ from cluster import *
 def build_face_image_points(X, y) :
     """
     Translate images to (labeled) points.
-    
+
     Parameters
     --------------------
         X     -- numpy array of shape (n,d), features (each row is one image)
         y     -- numpy array of shape (n,), targets
-    
+
     Returns
     --------------------
         point -- list of Points, dataset (one point for each image)
     """
-    
+
     n,d = X.shape
-    
+
     images = collections.defaultdict(list) # key = class, val = list of images with this class
     for i in xrange(n) :
         images[y[i]].append(X[i,:])
-    
+
     points = []
     for face in images :
         count = 0
@@ -64,7 +64,7 @@ def plot_clusters(clusters, title, average) :
                     determines how to calculate average of points in cluster
                     allowable: ClusterSet.centroids, ClusterSet.medoids
     """
-    
+
     plt.figure()
     np.random.seed(20)
     label = 0
@@ -86,21 +86,21 @@ def plot_clusters(clusters, title, average) :
 def generate_points_2d(N, seed=1234) :
     """
     Generate toy dataset of 3 clusters each with N points.
-    
+
     Parameters
     --------------------
         N      -- int, number of points to generate per cluster
         seed   -- random seed
-    
+
     Returns
     --------------------
         points -- list of Points, dataset
     """
     np.random.seed(seed)
-    
+
     mu = [[0,0.5], [1,1], [2,0.5]]
     sigma = [[0.1,0.1], [0.25,0.25], [0.15,0.15]]
-    
+
     label = 0
     points = []
     for m,s in zip(mu, sigma) :
@@ -108,7 +108,7 @@ def generate_points_2d(N, seed=1234) :
         for i in xrange(N) :
             x = util.random_sample_2d(m, s)
             points.append(Point(str(label)+'_'+str(i), label, x))
-    
+
     return points
 
 
@@ -119,36 +119,37 @@ def generate_points_2d(N, seed=1234) :
 def random_init(points, k) :
     """
     Randomly select k unique elements from points to be initial cluster centers.
-    
+
     Parameters
     --------------------
         points         -- list of Points, dataset
         k              -- int, number of clusters
-    
+
     Returns
     --------------------
         initial_points -- list of k Points, initial cluster centers
     """
     initial_points = []
+    newlist = list(points)
     for i in range(0, k):
-        choice = np.random.choice(len(points))
-        initial_points.append(points.pop(choice))
+        choice = np.random.choice(len(newlist))
+        initial_points.append(newlist.pop(choice))
     return initial_points
 
 
 def cheat_init(points) :
     """
     Initialize clusters by cheating!
-    
+
     Details
     - Let k be number of unique labels in dataset.
     - Group points into k clusters based on label (i.e. class) information.
     - Return medoid of each cluster as initial centers.
-    
+
     Parameters
     --------------------
         points         -- list of Points, dataset
-    
+
     Returns
     --------------------
         initial_points -- list of k Points, initial cluster centers
@@ -163,7 +164,7 @@ def cheat_init(points) :
 def kMeans(points, k, init='random', plot=False) :
     """
     Cluster points into k clusters using variations of k-means algorithm.
-    
+
     Parameters
     --------------------
         points  -- list of Points, dataset
@@ -172,16 +173,19 @@ def kMeans(points, k, init='random', plot=False) :
                    determines how to calculate average of points in cluster
                    allowable: ClusterSet.centroids, ClusterSet.medoids
         init    -- string, method of initialization
-                   allowable: 
+                   allowable:
                        'cheat'  -- use cheat_init to initialize clusters
                        'random' -- use random_init to initialize clusters
         plot    -- bool, True to plot clusters with corresponding averages
                          for each iteration of algorithm
-    
+
     Returns
     --------------------
         k_clusters -- ClusterSet, k clusters
     """
+    return kAverages(points, k, ClusterSet.centroids, init, plot)
+
+def kAverages(points, k, average, init, plot):
     k_clusters = ClusterSet()
     if init=='random':
         initSet = random_init(points, k)
@@ -203,17 +207,20 @@ def kMeans(points, k, init='random', plot=False) :
     iteration = 0
     while True:
         if plot:
-            plot_clusters(k_clusters, "kMeans "+init+" iteration "+str(iteration), ClusterSet.centroids)
+            if average==ClusterSet.centroids:
+                plot_clusters(k_clusters, "kMeans "+init+" iteration "+str(iteration), average)
+            elif average==ClusterSet.medoids:
+                plot_clusters(k_clusters, "kMedoids "+init+" iteration "+str(iteration), average)
         iteration+=1
         newClusters = ClusterSet()
         clusterMap = {}
-        centroids = k_clusters.centroids()
-        for c in centroids:
+        updatedCenters = average(k_clusters)
+        for c in updatedCenters:
             clusterMap[c]=[]
         for p in points:
             closest = None
             distance = float('inf')
-            for c in centroids:
+            for c in updatedCenters:
                 if p.distance(c)<distance:
                     closest=c
                     distance=p.distance(c)
@@ -226,17 +233,12 @@ def kMeans(points, k, init='random', plot=False) :
             k_clusters=newClusters
     return k_clusters
 
-
 def kMedoids(points, k, init='random', plot=False) :
     """
     Cluster points in k clusters using k-medoids clustering.
     See kMeans(...).
     """
-    ### ========== TODO : START ========== ###
-    # part e: implement
-    k_clusters = ClusterSet()
-    return k_clusters
-    ### ========== TODO : END ========== ###
+    return kAverages(points, k, ClusterSet.medoids, init, plot)
 
 
 ######################################################################
@@ -244,15 +246,15 @@ def kMedoids(points, k, init='random', plot=False) :
 ######################################################################
 
 def main() :
-    
+
     ### ========== TODO : START ========== ###
     # part d, part e, part f: cluster toy dataset
     np.random.seed(1234)
     points = generate_points_2d(20)
     kMeans(points, 3, init='random', plot=True)
     ### ========== TODO : END ========== ###
-    
-    
+
+
 
 
 if __name__ == "__main__" :
